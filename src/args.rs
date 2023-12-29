5bfbfa28994755cc08d9e9143ecbb0101230ba8d
timvisee <tim@visee.me>
2023-12-29 15:43:55 +0100

    Add argument parsing

---
+++ src/args.rs
@@ -0,0 +1,109 @@
+extern crate clap;
+extern crate num_cpus;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, disable_help_flag = true)]
+pub struct Arguments {
+    // manually redefine help, but without short option, because `-h`
+    // is already used by the height option.
+    /// Show this help
+    #[clap(long, action = clap::ArgAction::HelpLong)]
+    help: Option<bool>,
+
+    /// The host to pwn "host:port"
+    host: String,
+
+    /// Screen number (X11 ID)
+    #[arg(
+        short,
+        long,
+        value_name = "SCREEN_ID",
+        required = true,
+        default_value_t = 0
+    )]
+    screen: usize,
+
+    /// Draw width [default: screen width]
+    #[arg(short, long, value_name = "PIXELS")]
+    width: Option<u16>,
+    /// Draw height [default: screen height]
+    #[arg(short, long, value_name = "PIXELS")]
+    height: Option<u16>,
+
+    /// Draw X offset
+    #[arg(short, value_name = "PIXELS", default_value_t = 0)]
+    x: u16,
+    /// Draw Y offset
+    #[arg(short, value_name = "PIXELS", default_value_t = 0)]
+    y: u16,
+
+    /// Number of concurrent threads [default: number of CPUs]
+    #[arg(short, long, aliases = ["thread", "threads"])]
+    count: Option<usize>,
+
+    /// Use binary mode to set pixels (`PB` protocol extension) [default: off]
+    #[arg(short, long, alias = "bin")]
+    binary: bool,
+
+    /// Do not flush socket after each pixel [default: on]
+    #[arg(short, long)]
+    no_flush: bool,
+}
+
+/// CLI argument handler.
+pub struct ArgHandler {
+    data: Arguments,
+}
+
+impl ArgHandler {
+    pub fn parse() -> ArgHandler {
+        ArgHandler {
+            data: Arguments::parse(),
+        }
+    }
+
+    /// Get the host property.
+    pub fn host(&self) -> &str {
+        self.data.host.as_str()
+    }
+
+    /// Get the thread count.
+    pub fn count(&self) -> usize {
+        self.data.count.unwrap_or_else(num_cpus::get)
+    }
+
+    /// Get the screen ID.
+    pub fn screen(&self) -> usize {
+        self.data.screen
+    }
+
+    /// Get the image size.
+    /// Use the given default value if not set.
+    pub fn size(&self, def: Option<(u16, u16)>) -> (u16, u16) {
+        (
+            self.data
+                .width
+                .unwrap_or(def.expect("No screen width set or known").0),
+            self.data
+                .height
+                .unwrap_or(def.expect("No screen height set or known").1),
+        )
+    }
+
+    /// Get the image offset.
+    pub fn offset(&self) -> (u16, u16) {
+        (self.data.x, self.data.y)
+    }
+
+    /// Whether to use binary mode.
+    pub fn binary(&self) -> bool {
+        self.data.binary
+    }
+
+    /// Whether to prevent flushing after each pixel.
+    pub fn no_flush(&self) -> bool {
+        self.data.no_flush
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-const OFFSETX: usize = 50;
-const OFFSETY: usize = 50;
-const PX: usize = 200;
-const PY: usize = 150;
-const HOST: &str = "151.217.30.160:1234";
-const THREADS: usize = 32;
+pub mod args;
+
+// The default buffer size for reading the client stream.
+// - Big enough so we don't have to expand
+// - Small enough to not take up to much memory
+const CMD_READ_BUFFER_SIZE: usize = 32;
+
+// The response format of the screen size from a pixelflut server.
+const PIX_SERVER_SIZE_REGEX: &str = r"^(?i)\s*SIZE\s+([[:digit:]]+)\s+([[:digit:]]+)\s*$";
 
 use std::{
-    io::Write,
+    io::{BufRead, Error, ErrorKind, Write},
     mem,
     net::TcpStream,
     ops::Range,
@@ -14,44 +17,58 @@ use std::{
     thread,
 };
 
+use args::ArgHandler;
+use bufstream::BufStream;
 use captrs::{Bgr8, Capturer};
+use regex::Regex;
 
 fn main() {
-    let mut cap = Capturer::new(0).expect("failed to create capturer");
+    let args = Arc::new(ArgHandler::parse());
 
-    cap.capture_store_frame().expect("failed to capture frame");
+    println!("Opening screen capturer...");
+
+    let cap = Capturer::new(args.screen()).expect("failed to create capturer");
+
+    // Gather facts about the host
+    let screen_size =
+        gather_host_facts(&args).expect("Failed to gather facts about pixelflut server");
 
     let (gx, gy) = cap.geometry();
 
-    let factorx = gx as f32 / PX as f32;
-    let factory = gy as f32 / PY as f32;
+    let (outx, outy) = args.size(Some(screen_size));
+
+    let factorx = gx as f32 / outx as f32;
+    let factory = gy as f32 / outy as f32;
 
     let frame = vec![unsafe { mem::zeroed() }; gx as usize * gy as usize];
     let frame = Arc::new(RwLock::new(frame));
 
-    let vsync = Arc::new(Barrier::new(THREADS + 1));
+    let thread_count = args.count();
+    let vsync = Arc::new(Barrier::new(thread_count + 1));
 
-    let mut handles = Vec::new();
-    for i in 0..THREADS {
+    for i in 0..thread_count as u16 {
+        let args = args.clone();
         let shared_frame = frame.clone();
         let vsync = vsync.clone();
 
-        let starty = (PY as f32 / THREADS as f32) as usize * i;
-        let endy = (PY as f32 / THREADS as f32) as usize * (i + 1);
+        let starty = (outy as f32 / thread_count as f32) as u16 * i;
+        let endy = (outy as f32 / thread_count as f32) as u16 * (i + 1);
 
-        let handle = thread::spawn(move || {
+        thread::spawn(move || {
             painter(
-                shared_frame.clone(),
+                args,
+                shared_frame,
                 vsync,
                 factorx,
                 factory,
+                outx,
                 gx,
                 starty..endy,
             );
         });
-
-        handles.push(handle);
     }
+
+    println!("Starting... (use CTRL+C to stop)");
 
     capturer(cap, frame, vsync);
 }
@@ -70,14 +87,16 @@ fn capturer(mut cap: Capturer, shared_frame: Arc<RwLock<Vec<Bgr8>>>, vsync: Arc<
 }
 
 fn painter(
+    args: Arc<ArgHandler>,
     frame: Arc<RwLock<Vec<Bgr8>>>,
     vsync: Arc<Barrier>,
     factorx: f32,
     factory: f32,
+    outx: u16,
     gx: u32,
-    y_range: Range<usize>,
+    y_range: Range<u16>,
 ) {
-    let mut stream = TcpStream::connect(HOST).expect("failed to connect");
+    let mut stream = TcpStream::connect(args.host()).expect("failed to connect");
 
     loop {
         vsync.wait();
@@ -85,15 +104,15 @@ fn painter(
         let frame = frame.read().unwrap();
 
         for y in y_range.clone() {
-            for x in 0..PX {
+            for x in 0..outx {
                 let framex = (x as f32 * factorx) as u32;
                 let framey = (y as f32 * factory) as u32;
                 let pos = framey * gx + framex;
 
                 let pix = frame[pos as usize];
 
-                let x = x + OFFSETX;
-                let y = y + OFFSETY;
+                let x = x + args.offset().0;
+                let y = y + args.offset().1;
 
                 let msg = &[
                     b'P',
@@ -112,4 +131,46 @@ fn painter(
             }
         }
     }
+}
+
+/// Gather important facts about the host.
+fn gather_host_facts(args: &ArgHandler) -> Result<(u16, u16), Error> {
+    let mut stream = BufStream::new(TcpStream::connect(args.host()).expect("failed to connect"));
+    stream
+        .write_all("SIZE\n".as_bytes())
+        .expect("failed to request SIZE from screen");
+    stream
+        .flush()
+        .expect("failed to flush stream to request screen size");
+
+    // Build a regex to parse the screen size
+    let re = Regex::new(PIX_SERVER_SIZE_REGEX).unwrap();
+
+    // Read the output
+    // TODO: this operation may get stuck (?) if nothing is received from the server
+    let mut response = String::with_capacity(CMD_READ_BUFFER_SIZE);
+    stream.read_line(&mut response)?;
+
+    // Find captures in the data, return the result
+    let size = match re.captures(&response) {
+        Some(matches) => (
+            matches[1]
+                .parse::<u16>()
+                .expect("Failed to parse screen width, received malformed data"),
+            matches[2]
+                .parse::<u16>()
+                .expect("Failed to parse screen height, received malformed data"),
+        ),
+        None => {
+            return Err(Error::new(
+                ErrorKind::Other,
+                "Failed to parse screen size, received malformed data",
+            ))
+        }
+    };
+
+    // Print status
+    println!("Gathered screen size: {}x{}", size.0, size.1);
+
+    Ok(size)
 }
